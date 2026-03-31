824c6a3e7108b624b476d8774c9823d99a18d408
Nino <samuelnunessaldanha2007@gmail.com>
2026-03-31 10:53:56 -0300

    New Topic NullPointerException

---
+++ NullPointerException/Return empty string when argument is null/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+
+    public static String toUpperCase(String str) {
+        if (str == null) {
+            return "";
+        }
+        return str.toUpperCase(Locale.ENGLISH);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        line = "none".equalsIgnoreCase(line) ? null : line;
+        System.out.println(toUpperCase(line));
+    }
+}
